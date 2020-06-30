0e8817f8695a0d72db11eb170d1e7b7a219fd7c4
piyushsi <piyushsinha2309@gmail.com>
2020-06-30 07:02:09 +0530

    1.0.2

--- index.js
+++ index.js
@@ -5,29 +5,36 @@ const fetch = (id) => {
       return res.text();
     })
     .then((res) => {
-      var url = res
-        .split("profile_pic_url_hd")[1]
-        .split("requested_by_viewer")[0]
-        .split('"')[2]
-        .split("\\");
+      if (res.includes("profile_pic_url_hd")) {
+        var url = res
+          .split("profile_pic_url_hd")[1]
+          .split("requested_by_viewer")[0]
+          .split('"')[2]
+          .split("\\");
 
-      var hd_image_url =
-        url[0] +
-        "&" +
-        url[1].substr(5) +
-        "&" +
-        url[2].substr(5) +
-        "&" +
-        url[3].substr(5);
+        var hd_image_url =
+          url[0] +
+          "&" +
+          url[1].substr(5) +
+          "&" +
+          url[2].substr(5) +
+          "&" +
+          url[3].substr(5);
 
-      var title = res.split("<title>")[1].split("</title>")[0];
-      var name = title.split("(")[0];
-      var meta = res.split("<meta content=")[1].split(' name="description"')[0];
-      var followers = meta.split("Followers")[0].substr(1);
-      var following = meta.split("Following")[0].split(" ")[2];
-      var posts = meta.split("Posts")[0].split(" ")[4];
-	  var bio = res.split('"biography":"')[1].split('","blocked_')[0];
-      return { name, hd_image_url, followers, following, posts,bio };
+        var title = res.split("<title>")[1].split("</title>")[0];
+        var name = title.split("(")[0];
+        var meta = res
+          .split("<meta content=")[1]
+          .split(' name="description"')[0];
+        var followers = meta.split("Followers")[0].substr(1);
+        var following = meta.split("Following")[0].split(" ")[2];
+        var posts = meta.split("Posts")[0].split(" ")[4];
+        var bio = res.split('"biography":"')[1].split('","blocked_')[0];
+        return { name, hd_image_url, followers, following, posts, bio };
+      }
+      else {
+        return 'Profile not found'
+      }
     });
 };
 module.exports = {
